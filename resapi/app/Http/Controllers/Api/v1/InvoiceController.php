36aab305cacd53bf9a4d4c6d4e4eed2f993e85f8
tri-white <olegovic101@gmail.com>
2023-12-23 16:20:53 +0200

    added resources and pagination for Invoices

--- resapi/app/Http/Controllers/Api/v1/InvoiceController.php
+++ resapi/app/Http/Controllers/Api/v1/InvoiceController.php
@@ -6,7 +6,8 @@ use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Controllers\Controller;
-
+use App\Http\Resources\v1\InvoiceResource;
+use App\Http\Resources\v1\InvoiceCollection;
 class InvoiceController extends Controller
 {
     /**
@@ -14,7 +15,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        return Invoice::all();
+        return new InvoiceCollection(Invoice::paginate());
     }
 
     /**
@@ -38,7 +39,7 @@ class InvoiceController extends Controller
      */
     public function show(Invoice $invoice)
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
