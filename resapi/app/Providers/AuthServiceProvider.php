a2b0a4db1927b9cb3b0632c4a412a07e916f146f
tri-white <olegovic101@gmail.com>
2023-12-29 18:24:59 +0200

    all preconfigs are made here. not to the real deal

--- resapi/app/Providers/AuthServiceProvider.php
+++ resapi/app/Providers/AuthServiceProvider.php
@@ -21,6 +21,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
     }
 }
